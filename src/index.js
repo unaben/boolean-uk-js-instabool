35b761f10362e480f6e23978e378973d304a6887
Benedict Unachukwu <unaben@yahoo.com>
2021-08-03 19:39:09 +0100

    addEventListener , patch and likes

--- src/index.js
+++ src/index.js
@@ -1,65 +1,118 @@
-// write your code here
-
-let images = []
-
 fetch("http://localhost:3000/images")
-  .then((res) => res.json())
-  .then((images) => {
-    console.log("Inside GET Fetch: ", images);
+.then((res) => res.json())
+.then((images) => {
+  console.log("Inside GET Fetch: ", images);
 
-    renderCardImage(images)
-  });
+  renderCardImage(images)
+});
 
-console.log("images: ", images)
 const containerEl = document.querySelector(".image-container")
+console.log("conrainerEl: ", containerEl)
 
 function renderCardImage (datas) {
 
-  for (let i = 0; i < datas.length; i++){
+for (let i = 0; i < datas.length; i++){
 const data = datas[i]
-console.log("inside data: ", data)
+// console.log("inside data: ", data)
 
 const articleEl = document.createElement("article")
 articleEl.className = "image-card"
 containerEl.append(articleEl)
 console.log("articleEl: ", articleEl)
 
-const titleEl = document.createElement("h2")
-titleEl.className = "title"
-titleEl.innerText = data.title
-articleEl.append(titleEl)
+const headingEl = document.createElement("h2")
+headingEl.className = "title"
+headingEl.innerText = data.title
+articleEl.append(headingEl)
 
 const imageEl = document.createElement("img")
 imageEl.src = data.image
 imageEl.className = "image"
 articleEl.append(imageEl)
 
-const frameEl = document.createElement("div")
-frameEl.className = "likes-section"
-articleEl.append(frameEl)
+const divEl = document.createElement("div")
+divEl.className = "likes-section"
+articleEl.append(divEl)
 
 const spanEl = document.createElement("span")
 spanEl.className = "likes"
 spanEl.innerText = data.likes
-frameEl.append(spanEl)
+divEl.append(spanEl)
 
 const buttonEl = document.createElement("button")
 buttonEl.className = "like-button"
 buttonEl.innerText = "♥"
-frameEl.append(buttonEl)
 
-const ulEl = document.createElement("ul")
-ulEl.className = "comments"
-articleEl.append(ulEl)
+buttonEl.addEventListener("click", () => {
+console.log("clicked!: ", data.id, data.likes)
 
-const listEl = document.createElement("li")
+const likesToUpdate = {
+
+  likes: data.likes + 1
+}
+const fetchOptions = {
+  method: "PATCH",
+  headers: {
+    "Content-Type": "application/json"
+  },
+  body: JSON.stringify(likesToUpdate)
+}
+
+fetch(`http://localhost:3000/images/${data.id}`, fetchOptions)
+  .then((res) => res.json())
+  .then((updatedLikes) => {
+    console.log("Inside PATCH Fetch: ", updatedLikes);
+  })
+});
+divEl.append(buttonEl)
+
+// const like = fetch('http://localhost:3000/data.like/1', {
+//   method: 'DELETE'
+// })
+// console.log(like)
+
+
+
+// likesButton.addEventListener("click", () => {
+//   console.log("clicked! ", image.id, image.likes)
+
+  // You need access to the "id" and the current "likes" of an image/post
+
+  // Write our fetch request in here...
+// })
+
+const listEl = document.createElement("ul")
+listEl.className = "comments"
+articleEl.append(listEl)
 
 data.comments.forEach((item) => {
-    const listEl = document.createElement("li")
-    listEl.innerText = item.content
-    ulEl.append(listEl)
+  const listItemEl = document.createElement("li")
+  listItemEl.innerText = item.content
+  listEl.append(listItemEl)
 });
-}
-}
 
- 
+const formEl = document.createElement("form")
+formEl.className = "comment-form", "image-card"
+containerEl.append(formEl)
+
+const commentFormEl = document.createElement("form")
+commentFormEl.className = "comment-form"
+articleEl.append(commentFormEl)
+
+const inputEl = document.createElement("input")
+inputEl.className = "comment-input"
+inputEl.type = "text"
+inputEl.name = "comment"
+inputEl.placeholder = "Add a comment..."
+commentFormEl.append(inputEl)
+
+const btnEl = document.createElement("button")
+btnEl.className = "comment-button"
+btnEl.type = "submit"
+btnEl.innerText = "Post"
+btnEl.addEventListener("click", () => {;
+console.log("Inside btnEl: ", btnEl)
+});
+commentFormEl.append(btnEl)
+}
+}
